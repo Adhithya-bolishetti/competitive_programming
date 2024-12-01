b207974a262ded3b7cf8b2ea738d1ed2e1dc333a
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2024-12-01 23:29:27 +0530

    Updated

--- leetcode-1346.java
+++ leetcode-1346.java
@@ -4,13 +4,14 @@ class Solution {
         HAshMap<Integer, Integer> map=new HashMap();
         
         for(int i=0; i<n; i++){
-            if( map.containsKey(arr[i]*2)){
+            int val=arr[i];
+            if( map.containsKey(val*2)){
                 return true;
             }
-            else if(arr[i]%2==0 && map.containsKey(arr[i]/2)){
+            else if(val%2==0 && map.containsKey(val/2)){
                 return true;
             }
-            map.put(arr[i], map.getOrDefault(arr[i],0)+1);            
+            map.put(val, map.getOrDefault(val,0)+1);            
         }
 
 
