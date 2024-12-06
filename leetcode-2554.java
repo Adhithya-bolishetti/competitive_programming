9640e1563b19272e597f7f58556542ab7a0caf78
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2024-12-06 19:32:01 +0530

    Initial commit

---
+++ leetcode-2554.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        HashSet <Integer> hs=new HashSet<>();
+        for(int i=0;i<banned.length;i++){
+            hs.add(banned[i]);
+        }
+        int sum=0;
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(!hs.contains(i)){
+                System.out.print(i);
+                sum+=i;
+                 if(sum>maxSum) break;
+                count++;
+            }
+        }
+        return count;
+    }
+}
