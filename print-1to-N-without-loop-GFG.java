630e23654471f313dd904e645336c2f3f23f5863
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2024-12-27 10:48:11 +0530

    Organized files

--- print-1to-N-without-loop-GFG.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-
-    public void printNos(int n) {
-        // Your code here
-        if(n==0) return;
-        printNos(n-1);
-        System.out.print(n+" ");
-    }
-}
--- print-GFG-N-times-GFG.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-
-    void printGfg(int N) {
-        // code here
-        if(N<1) return;
-        System.out.print("GFG ");
-        printGfg(N-1);
-    }
-}
