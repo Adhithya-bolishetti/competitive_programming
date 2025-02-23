d4ff966d8167a133103a345600b3f65ce887c509
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-02-23 17:00:12 +0530

    Initial commit

---
+++ Geeks_for_Geeks/missing_and_repeating(optimal)_GFG.java
@@ -0,0 +1,36 @@
+class Solution {
+    // Function to find two elements in array
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        // code here
+        long n = arr.length; // size of the array
+        // Find Sn and S2n:
+        long SN = (n * (n + 1)) / 2;
+        long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+        // Calculate S and S2:
+        long S = 0, S2 = 0;
+        for (int i = 0; i < n; i++) {
+            S += arr[i];
+            S2 += (long)arr[i] * (long)arr[i];
+        }
+
+        //S-Sn = X-Y:
+        long val1 = S - SN;
+
+        // S2-S2n = X^2-Y^2:
+        long val2 = S2 - S2N;
+
+        //Find X+Y = (X^2-Y^2)/(X-Y):
+        val2 = val2 / val1;
+
+        //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+        // Here, X-Y = val1 and X+Y = val2:
+        long x = (val1 + val2) / 2;
+        long y = x - val1;
+
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add((int)x);
+        list.add((int)y);
+        return list;
+    }
+}
