7444097218695cc90abc963cea9bf7875a194e4d
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-03-13 22:25:32 +0530

    Modified code

--- leetcode/leetcode-1011.java
+++ leetcode/leetcode-1011.java
@@ -1,34 +1,35 @@
 class Solution {
-    public boolean isPossible(int[] weights,int days,int k){
-        int temp=0;
-        for(int i=0;i<weights.length;i++){
-            int val=weights[i];
-            if(val>k){
-                return false;
-            }
-            if(temp+val>k){
-                days--;
-                temp=0;
-            }
-            temp+=val;
-            if(days<=0){
-                return false;
-            }
-        }
-        return true;
-    }
-    public int shipWithinDays(int[] weights, int days) {
-        int l=1;
-        int r=Integer.MAX_VALUE;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(isPossible(weights,days,mid)){
-                r=mid-1;
+    public int isPossible(int[] weights,int n,int mid){
+        int sum=0;
+        int Ndays=1;
+        for(int i=0;i<n;i++){
+            if(sum+weights[i]>mid){
+                Ndays+=1;
+                sum=weights[i];
             }
             else{
-                l=mid+1;
+                sum+=weights[i];
             }
         }
-        return l;
+        return Ndays;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int n=weights.length;
+        int low=Integer.MIN_VALUE;
+        int high=0;
+        for(int i=0;i<n;i++){
+            high+=weights[i];
+            low=Math.max(low,weights[i]);
+        }
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isPossible(weights,n,mid)<=days){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
     }
 }
