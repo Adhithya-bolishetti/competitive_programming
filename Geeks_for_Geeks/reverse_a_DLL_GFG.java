964c8cdc0037c3a39bbf25af58bc4097bb67176a
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-03-31 12:46:11 +0530

    Modified code

--- Geeks_for_Geeks/reverse_a_DLL_GFG.java
+++ Geeks_for_Geeks/reverse_a_DLL_GFG.java
@@ -4,10 +4,15 @@ class Solution {
         DLLNode temp = head;
         DLLNode newHead = null;
         while(temp != null) {
+            // Swap next and prev pointers
+            // for the current node
             DLLNode prevNode = temp.prev;
             temp.prev = temp.next;
             temp.next = prevNode;
+            //assign newHead to the last node
+            // of the original list
             newHead = temp;
+            // Move to the next node in the original list
             temp = temp.prev;
         }
         return newHead;
