9c299cacfe189d9fd395a21ee67f880310c74d04
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-04-11 22:58:01 +0530

    Refactor prime factors calculation to use isPrime method

--- prime_factors.java
+++ prime_factors.java
@@ -1,19 +1,22 @@
 import java.util.*;
-public class prime_factors
-{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for(int i=1;i<=n;i++) {
-		    if(n%i == 0) {
-		        int count = 0;
-		        if(i>=2) {
-		            for(int j=1;j<=i;j++) {
-		                if(i%j == 0) count++;
-		            }
-		        }
-		        if(count == 2) System.out.print(i+" ");
-		    }
-		}
-	}
+
+public class prime_factors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0 && isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
 }
