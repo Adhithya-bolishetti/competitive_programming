4af9218aec4828f18c895af7b709ea0f422a45f5
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-04-18 22:24:55 +0530

    Add implementation of nextGreaterElement method to find the next greater element for each element in nums1 from nums2

---
+++ leetcode/leetcode-496.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+        for(int i=0;i<ans.length;i++) {
+            ans[i] = -1;
+        }
+        for(int i=0;i<nums1.length;i++) {
+            int max = Integer.MIN_VALUE;
+            for(int j=nums2.length-1;j>=0;j--) {
+                if(nums2[j] == nums1[i]) break;
+                if(nums2[j] > nums1[i]) {
+                    max = nums2[j];
+                    ans[i] = max;
+                }
+            }
+        }
+        return ans;
+    }
+}
