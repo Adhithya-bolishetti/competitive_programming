a35d1412a6faf92b36fc44823e24410f26bcdf32
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-04-18 20:33:58 +0530

    Add implementation of a method to convert postfix expressions to prefix notation

---
+++ PostfixToPrefix.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class PostfixToPrefix {
+
+    public static String postfixToPrefix(String s) {
+        StringBuilder sb = new StringBuilder();
+        Stack<String> st = new Stack<>();
+        int i = 0;
+        while(i<s.length()) {
+            char c = s.charAt(i);
+            if(Character.isLetterOrDigit(c)) {
+                st.push(String.valueOf(c));
+            }
+            else {
+                String t1 = st.peek();
+                st.pop();
+                String t2 = st.peek();
+                st.pop();
+                st.push(String.valueOf(c)+t2+t1);
+            }
+            i++;
+        }
+        return st.peek();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Postfix expression:");
+        String s = sc.nextLine();
+        String ans = postfixToPrefix(s);
+        System.out.println("Prefix expression: \n" + ans);
+    }
+}
