5be98ad387c3dc946323fb20e188d896b258265e
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-05-10 20:28:44 +0530

    Add implementation of merge sort algorithm to count inversions in an array

---
+++ CRT/inversionsusingMergeSort.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class InversionsusingMergeSort {
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int[] left = Arrays.copyOfRange(arr, low, mid + 1);
+        int[] right = Arrays.copyOfRange(arr, mid + 1, high + 1);
+
+        int i = 0, j = 0, k = low;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+                // All remaining elements in left[] form inversion with right[j-1]
+                for (int l = i; l < left.length; l++) {
+                    System.out.println("(" + left[l] + ", " + right[j - 1] + ")");
+                }
+            }
+        }
+
+        // Copy remaining elements
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) return;
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        mergeSort(arr, 0, n - 1);
+    }
+}
