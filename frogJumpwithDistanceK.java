e55a95ec5bf2462b2ac7db5092330582581f85dd
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-05-23 15:52:08 +0530

    Add implementation for frog jump with k distances using recursion

---
+++ frogJumpwithDistanceK.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static int minEnergy(int[] arr, int index, int k) {
+        if (index == 0) return 0;
+        int mmSteps = Integer.MAX_VALUE;
+        for(int j=1; j<=k; j++) {
+             if (index - j >= 0) {
+                int jump = minEnergy(arr,index - j, k) + Math.abs(arr[index] - arr[index - j]);
+                mmSteps = Math.min(jump, mmSteps);
+            }
+        }
+
+        return mmSteps;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int k = sc.nextInt();
+        int ans = minEnergy(arr, n - 1, k);
+        System.out.print(ans);
+    }
+}
+// Time complexity : O(n^k)
+// Space complexity : O(n)
