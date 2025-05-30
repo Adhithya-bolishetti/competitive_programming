2dd05d1e2964154d5a5ed9633c1c8ba37efbeb8d
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-05-30 23:28:17 +0530

    Add recursion solution to find the possible paths without any obstacles

--- leetcode/leetcode_63.java
+++ leetcode/leetcode_63.java
@@ -1,16 +1,23 @@
 class Solution {
     public int countPaths(int[][] obstacleGrid, int m, int n) {
-        if(m == 0 && n == 0) return 1;
-        if(m < 0 || n < 0) return 0;
-        if(obstacleGrid[m][n] == 1) return 0;
-        int left = countPaths(obstacleGrid, m-1, n);
-        int up = countPaths(obstacleGrid, m, n-1);
+        if (m < 0 || n < 0) return 0;
+        if (obstacleGrid[m][n] == 1) return 0;
+        if (m == 0 && n == 0) return 1;
+
+        int left = countPaths(obstacleGrid, m - 1, n);
+        int up = countPaths(obstacleGrid, m, n - 1);
+
         return left + up;
     }
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid[0][0] == 1) return 0;
-        else  if (obstacleGrid[0][0] == 1) return 1;
-        else return countPaths(obstacleGrid, obstacleGrid.length-1, obstacleGrid[0].length-1);
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        // Base case: if the start or end is blocked, no path exists
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0;
+
+        return countPaths(obstacleGrid, m - 1, n - 1);
     }
 }
 // Time complexity : O(2^(m*n))
