b0efb7b0d946065a127631119e218ec3b63636b8
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-06-01 21:19:47 +0530

    Updated recursive solution to find total chocolates collected by Alice and Bob

--- code360/chocolatePickup.java
+++ code360/chocolatePickup.java
@@ -1,42 +1,35 @@
+import java.util.*; 
+import java.io.*; 
 
+public class Solution {
+	static int maxChocoUtil(int i, int j1, int j2, int m, int n, int[][] grid) {
+		if (j1 < 0 || j1 >= n || j2 < 0 || j2 >= n)
+			return (int)(-1e9);
 
-class Main {
-  static int maxChocoUtil(int i, int j1, int j2, int m, int n, int[][] grid) {
-    if (j1 < 0 || j1 >= n || j2 < 0 || j2 >= n)
-      return (int) (Math.pow(-10, 9));
+		if (i == m - 1) {
+			if (j1 == j2) return grid[i][j1];
+			else return grid[i][j1] + grid[i][j2];
+		}
 
-    if (i == m - 1) {
-      if (j1 == j2)
-        return grid[i][j1];
-      else
-        return grid[i][j1] + grid[i][j2];
-    }
+		int maxi = Integer.MIN_VALUE;
 
+		for (int di = -1; di <= 1; di++) {
+			for (int dj = -1; dj <= 1; dj++) {
+				int ans;
+				if (j1 == j2)
+					ans = grid[i][j1] + maxChocoUtil(i + 1, j1 + di, j2 + dj, m, n, grid);
+				else
+					ans = grid[i][j1] + grid[i][j2] + maxChocoUtil(i + 1, j1 + di, j2 + dj, m, n, grid);
 
-    int maxi = Integer.MIN_VALUE;
-    for (int di = -1; di <= 1; di++) {
-      for (int dj = -1; dj <= 1; dj++) {
-        int ans;
-        if (j1 == j2)
-          ans = grid[i][j1] + maxChocoUtil(i + 1, j1 + di, j2 + dj, n, m, grid);
-        else
-          ans = grid[i][j1] + grid[i][j2] + maxChocoUtil(i + 1, j1 + di, j2 + dj, n, m, grid);
-        maxi = Math.max(maxi, ans);
-      }
-    }
-    return maxi;
-  }
+				maxi = Math.max(maxi, ans);
+			}
+		}
+		return maxi;
+	}
 
-
-  public static void main(String args[]) {
-    int matrix[][] = {{2, 3, 1, 2},
-                      {3, 4, 2, 2},
-                      {5, 6, 3, 5}};
-    int m = matrix.length;
-    int n = matrix[0].length;
-
-    System.out.println(maxChocoUtil(0, 0, n-1, m, n, matrix));
-  }
+	public static int maximumChocolates(int r, int c, int[][] grid) {
+		return maxChocoUtil(0, 0, c - 1, r, c, grid); 
+	}
 }
-// Time complexity : O(9^m)
-// Space complexity : O(m)
+// Time complexity : O(9^r)
+// Space complexity : O(r)
