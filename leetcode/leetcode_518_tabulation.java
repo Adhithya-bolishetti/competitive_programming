7d058fb45af4c5dd9abe691eff4fb5ce53609738
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-06-07 20:40:11 +0530

    Add dp tabulation solution find number of combinations that makes up the given amount

---
+++ leetcode/leetcode_518_tabulation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        
+        for(int i=0; i<= amount; i++) {
+            if(i % coins[0] == 0) dp[0][i] = 1;
+        }
+
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<=amount; j++) {
+                int notPick = dp[i-1][j];
+                int pick = 0;
+                if(j >= coins[i]) {
+                    pick = dp[i][j - coins[i]];
+                }
+                dp[i][j] = pick + notPick;
+            }
+        }
+        return dp[n-1][amount];
+    }
+}
+// Time complexity : O(n * amount)
+// Space complexity : O(n * amount)
