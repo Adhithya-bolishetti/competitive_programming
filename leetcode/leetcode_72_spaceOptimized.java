844c2c539599079bf9d2691ea0de97a3f4580584
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-06-14 15:37:49 +0530

    Add dp space optimized solution to return the minimum number of operations required to convert word1 to word2.

---
+++ leetcode/leetcode_72_spaceOptimized.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[] dp = new int[m+1];
+
+        for(int j=0; j<=m; j++) dp[j] = j;
+
+        for(int i=1; i<=n; i++) {
+            int[] temp = new int[m+1];
+            temp[0] = i;
+            for(int j=1; j<=m; j++) {
+                if(word1.charAt(i-1) == word2.charAt(j-1)) {
+                    temp[j] = dp[j-1];
+                } 
+                else {
+                    int insert = 1 + temp[j-1];
+                    int delete = 1 + dp[j];
+                    int replace = 1 + dp[j-1];
+                    temp[j] = Math.min(insert, Math.min(delete, replace));
+                }
+            }
+            dp = temp;
+        }
+
+        return dp[m];
+    }
+}
+// Time complexity : O(n * m)
+// Space complexity : O(m)
