2aec444f2cb459c2394013569b8bbca470835c55
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-07-23 16:46:12 +0530

    Add solution to return sum of elements in an array

---
+++ Geeks_for_Geeks/sum_of_aray_GFG.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+
+class Solution {
+    int arraySum(int arr[]) {
+        // code here
+        int sum = 0;
+        for(int i=0; i<arr.length; i++) {
+            sum += arr[i];
+        }
+        
+        return sum;
+    }
+}
+// Time complexity : O(n)
+// Space complexity : O(1)
