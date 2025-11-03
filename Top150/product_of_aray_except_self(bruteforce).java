9050bd73e23e350bdcb1160c618297ed80952a43
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-11-03 22:11:08 +0530

    Add brute force implementation of product of array except self

---
+++ Top150/product_of_aray_except_self(bruteforce).java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        for(int i=0;i<nums.length;i++) {
+            int product = 1;
+            for(int j=0;j<nums.length;j++) {
+                if(i != j) {
+                    product *= nums[j];
+                }
+            }
+            res[i] = product;
+        }
+
+        return res;
+    }
+}
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
