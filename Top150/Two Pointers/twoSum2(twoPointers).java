7bca1c0ed6d0cf811948c3b72c62b3fb0e02634c
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-11-04 12:00:18 +0530

    Add implementation of two sum problem using two pointers

---
+++ Top150/Two Pointers/twoSum2(twoPointers).java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0;
+        int r = numbers.length - 1;
+
+        while(l < r) {
+            while(numbers[l] + numbers[r] > target) {
+                r--;
+            }
+            while(numbers[l] + numbers[r] < target) {
+                l++;
+            }
+            if(numbers[l] + numbers[r] == target) {
+                return new int[]{l+1, r+1};
+            }
+        }
+
+        return new int[0];
+    }
+}
+// Time Complexity: O(n)
+// Space Complexity: O(1)
