4cc21d75d5750e2fe4fc4561a951c8a4376c2164
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-11-08 20:15:58 +0530

    Add sliding window implementation for minimum window substring problem

---
+++ Top150/Sliding Window/Minimum_window_substring(sliding window).java
@@ -0,0 +1,32 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(s.length() < t.length()) return "";
+        int[] hash = new int[256];
+        for(int i=0;i<t.length();i++) {
+            hash[t.charAt(i)]++;
+        }
+
+        int sindex = -1;
+        int count = 0;
+        int minlen = Integer.MAX_VALUE;
+        int l = 0;
+        for(int r=0;r<s.length();r++) {
+            if(hash[s.charAt(r)] > 0) count++;
+            hash[s.charAt(r)]--;
+
+            while(count == t.length()) {
+                if((r - l + 1) < minlen) {
+                    minlen = r - l + 1;
+                    sindex = l;
+                }
+                hash[s.charAt(l)]++;
+                if(hash[s.charAt(l)] > 0) count--;
+                l++;
+            }
+        }
+
+        return sindex == -1 ? "" : s.substring(sindex, sindex + minlen);
+    }
+}
+// Time complexity: O(N * M) where N is length of s and M is length of t
+// Space complexity: O(1) since hash array size is constant
