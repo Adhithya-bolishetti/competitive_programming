7de5effdecb1c2310ec2752d5ec96f18e2327d81
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-11-10 16:57:19 +0530

    Add brute force implementation to find index of peak element in an array

---
+++ Top150/Binary Search/find_peak_element(bruteforce).java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+        
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i] > max) {
+                max = nums[i];
+                index = i;
+            }
+        }
+
+        return index;
+    }
+}
+//Time Complexity: O(n)
+//Space Complexity: O(1)
