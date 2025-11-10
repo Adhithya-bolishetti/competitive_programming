2c7b1921f4a2e7e3aea15b96623265d988d24a17
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2025-11-10 23:19:05 +0530

    Add binary search implementation to find first and last position of an element in sorted array

---
+++ Top150/Binary Search/Find_first&last_position_of_an_element(binary search).java
@@ -0,0 +1,49 @@
+class Solution {
+    public int leftMost(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] >= target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        if(l >= nums.length || nums[l] != target) {
+            return -1;
+        }
+
+        return l;
+    }
+
+    public int rightMost(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        if(r < 0 || nums[r] != target) {
+            return -1;
+        }
+
+        return r;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+       int first = leftMost(nums, target);
+       int last = rightMost(nums, target);
+       return new int[]{first, last};
+    }
+}
+//Time Complexity: O(log n)
+//Space Complexity: O(1)
