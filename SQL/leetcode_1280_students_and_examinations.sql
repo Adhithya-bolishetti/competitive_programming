ef28a79e54dfed0923801626f5cf83a213110f3e
Adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2026-01-09 10:24:27 +0530

    Add SQL implementations for student examination attendance, confirmation rate, and employee managers with at least five direct reports

---
+++ SQL/leetcode_1280_students_and_examinations.sql
@@ -0,0 +1,8 @@
+SELECT S1.student_id, S1.student_name, S2.subject_name, COUNT(E.student_id) AS attended_exams
+FROM Students S1
+CROSS JOIN Subjects AS S2
+LEFT JOIN Examinations AS E
+    ON S1.student_id = E.student_id
+    AND E.subject_name = S2.subject_name
+GROUP BY S1.student_id, S1.student_name, S2.subject_name
+ORDER BY S1.student_id, S1.student_name, S2.subject_name;
